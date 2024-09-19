402a9bdd9249d5d372109be609f5bf95e644614f
carolinascarvalho3 <carolsinha10@outlook.com>
2024-09-19 09:50:27 -0300

    Add files via upload
    Dia 19/09/2024

--- exercicios2.py
+++ exercicios2.py
@@ -67,13 +67,206 @@ class Exercicios2:
                 primo+=f'\n{i}'
         return primo
 
-    def primo(self, num1):
+    def valores(self, num1, num2):
+        num1 = 10
+        num2 = 20
+
+        num1, num2 = num2, num1
+
+    def antecessor(self):
         num1 = int(input('Digite um número: '))
-        for i in range(1, num1 + 1):
-            if num1 % i == 0:
-                print('É primo')
-            else:
-                print('Não é primo')
+
+        num2 = num1 - 1
+
+        return num2
+
+    def retangulo(self, base, altura, area):
+        base = int(input('Digite um número: '))
+        altura = int(input('Digite um número: '))
+
+        area = base * altura * altura
+        return area
+    def calculoIdade(self, idade, meses, anos, dias, nasc):
+        diasTot = (anos * 365) + (meses * 30) + dias
+
+        idade = ""
+        meses = ""
+        anos = ""
+        dias = ""
+
+        idade_em_dias = (anos, meses, dias)
+        return diasTot
+    def eleitores(self, brancos, nulos, validos, pBrancos, pNulos, pValidos, eleitores):
+
+        eleitores = int(input('Digite o número total de eleitores: '))
+        brancos = int(input('Digite o número total de brancos: '))
+        nulos = int(input('Digite o número total de nulos: '))
+        validos = int(input('Digite o número total de validos: '))
+
+        pBrancos = (brancos * 100) /eleitores
+        pNulos = (nulos * 100) /eleitores
+        pvalidos = (validos * 100) /eleitores
+
+    def salarioMensal(self, salAtu, novoSal, per):
+
+        salAtu = int(input('Informe o salário atual: '))
+        novoSal = salAtu + (salAtu *10/100)
+
+        return novoSal
+
+    def custoDecarro(self, custoFabrica, pDistribuidor,pImpostos, cutoCarro):
+
+       custoFabrica = ""
+       pDistribuidor = ""
+       pImpostos = ""
+       custoCarro = ""
+
+       custoFabrica = int(input('Qual o custo de fábrica: '))
+       pDistribuidor = int(pDistribuidor * 100) /custoFabrica
+       pImpostos = int(pImpostos * 100) /custoFabrica
+       custoCarro = (pImpostos + pDistribuidor + custoFabrica)
+
+       return custoCarro
+
+    def notas(self, nota1, nota2, nota3, media):
+
+        nota1 = int("")
+        nota2 = int("")
+        nota3 = int("")
+        media = int(nota1 + nota2 + nota3)/3
+
+        int(input('Digite a primeira nota: '))
+        int(input('Digite a segunda nota: '))
+        int(input('Digite a terceira nota: '))
+
+        if nota1 >= 8 and nota3 >=8:
+            'Aluno Aprovado'
+        if nota1 <8 and nota3 >=5:
+            'Alubo em Recuperação'
+        else: 'Aluno Reprovado'
+
+    def macas(self):
+
+        mac = int("")
+        duzia = float("")
+        total = float("")
+        num = ""
+
+        num = int(input('Quantas maçãs foram compradas? : '))
+
+        if mac < 12:
+            total = (mac * 1.30)
+        else: total = (duzia * 1.00)
+
+        return total
+
+    def lerValores(self, cont):
+        cont = 0
+        while True:
+         print(cont)
+         cont =+1
+         if cont >= 10:
+          break
+
+        return cont
+
+    def salarioVendedor(self, salFixo, pComissao, salTotal, totVendas):
+
+        salFixo = ""
+        pComissao = ""
+        salTotal = ""
+        totVendas = float("")
+
+        salFixo = (int(input('Digite o salario fixo')))
+
+        if totVendas == 1.500:
+            totVendas = (totVendas * 3) /100
+        pComissao = (totVendas * 3)/100
+
+        if totVendas > 1.500:
+            totVendas = (totVendas * 5)/100
+            salTotal = totVendas, salFixo
+            return salTotal
+
+    def extratoBanco(self):
+
+        contCli = int(input('Digite o número de sua conta e dígito'))
+        saldo = float(10.00)
+        debito = float(-50.00)
+        credito = float(1.430)
+
+        saldAtu = (saldo - debito + credito)
+        if saldAtu < 0:
+         return saldAtu, 'Saldo Negativo'
+        if saldAtu > 0:
+         return saldAtu, 'Saldo Positivo!'
+
+    def tabuadaUsuario(self):
+
+     num1 = int(input('Digite um número entre 1 a 10: '))
+     for i in range(1,11):
+        print(f'{num1} x {i} = {num1 * i}')
+        return i
+
+    def imprimirValores(self):
+
+        valor1 = int(input('Digite um valor: '))
+        cont = 1
+        while True:
+            print(cont)
+            cont = +1
+            if cont == valor1:
+                break
+
+        return cont
+
+    def ler10(self):
+
+       neg = 0
+       for i in range(1,11):
+        valor = float(input('Digite um valor: '))
+        if valor < 0:
+                neg =+ 1
+
+    def somar40(self):
+
+        soma = 0
+        numero = 0
+        for i in range(1,11):
+          numero = float(input('Digite um número: '))
+        if soma < 40 :
+          soma += numero
+
+    def mediaAritmetica(self):
+
+        mediaAri =  0
+        quantidade = 0
+
+        for i in range(15, 100):
+            mediaAri += i
+            quantidade += 1
+            media = mediaAri / quantidade
+
+    def quantidade(self):
+        quantidade = int(input('Digite a quantidade de números: '))
+
+        numeros = {}
+        soma = 0
+
+        for i in range(quantidade):
+            numero = int(input('Digite um números: '))
+
+            numeros[numero] = soma
+            soma += numero
+
+            media = soma / quantidade
+
+            maiorNumero = max(numeros)
+
+
+
+
+
 
 
 
--- menu2.py
+++ menu2.py
@@ -7,6 +7,7 @@ class Menu:
         self.num1  = 0
         self.num2  = 0
 
+
     def mostrarMenu2(self):
             print('\n------Menu-----\n\n'           +
                   'Escolha uma das opções abaixo: ' +
@@ -20,8 +21,24 @@ class Menu:
                   '\n7. Tabuada'                    +
                   '\n8. Tabuada 2'                  +
                   '\n9. Somar'                      +
-                  '\n10. Primos'                    +
-                  '\n11. Primo ou não Primo'        )
+                  '\n10. Primo'                     +
+                  '\n11. Valores'                   +
+                  '\n12. Antecessor'                +
+                  '\n13. Retângulo'                 +
+                  '\n14. Cálculo da Idade'          +
+                  '\n15. Eleitores'                 +
+                  '\n16. Salário Mensal'            +
+                  '\n17. Custo de um Carro'         +
+                  '\n18. Três Notas'                +
+                  '\n19. Maças Compradas'           +
+                  '\n20. Ler Valores'               +
+                  '\n21. Salário Vendedor'          +
+                  '\n22. Extrato de Banco'          +
+                  '\n23. Tabuada Usuário'           +
+                  '\n24. Imprimir Valores'          +
+                  '\n25. Ler 10'                    +
+                  '\n26. Somar abaixo de 40'        +
+                  '\n27. Media Aritmética'          )
     def coletar(self):
             self.num1 = int(input('Informe o primeiro número:  '))
             self.num2 = int(input('Informe o segundo número:  '))
@@ -59,5 +76,46 @@ class Menu:
             elif self.opcao == 10:
                 print(f'{self.exer.primo20()}')
             elif self.opcao == 11:
-                print(f'{self.exer.primo(self.num1)}')
+                num1 = int(input('Informe um número: '))
+                num2 = int(input('Informe um número: '))
+                print(f'{self.exer.valores(self.num1, self.num2)}')
+            elif self.opcao == 12:
+                print(f'O número é: {self.exer.antecessor()}')
+            elif self.opcao == 13:
+                 print(f'As dimensões do retângulo são: {self.exer.retangulo}')
+            elif self.opcao == 14:
+                print(f'O cálculo da idade em dias, meses e anos é: {self.exer.calculoIdade}')
+            elif self.opcao == 15:
+                print(f' O cálculo de votos é: {self.exer.eleitores}')
+            elif self.opcao == 16:
+                print(f'O salário com reajuste é: {self.exer.salarioMensal}')
+            elif self.opcao == 17:
+                print(f'O custo de um carro novo é: {self.exer.custoDecarro}')
+            elif self.opcao == 18:
+                print(f'A média das notas é: {self.exer.notas}')
+            elif self.opcao == 19:
+                print(f'O preço das maçãs compradas é: {self.exer.macas}')
+            elif self.opcao == 20:
+                print(f'{self.exer.lerValores}')
+            elif self.opcao == 21:
+                print(f'O salário total dos vendedores é: {self.exer.salarioVendedor}')
+            elif self.opcao == 22:
+                print(f'{self.exer.extratoBanco}')
+            elif self.opcao == 23:
+                print(f'{self.exer.tabuadaUsuario}')
+            elif self.opcao == 24:
+                print(f'{self.exer.imprimirValores}')
+            elif self.opcao == 25:
+                print(f'{self.exer.ler10}')
+            elif self.opcao == 26:
+                print(f'{self.exer.somar40}')
+            elif self.opcao == 27:
+                print(f'A média aritmética é dos números inteiros é: {self.exer.mediaAritmetica}')
+
+
+
+
+
+
+
 
